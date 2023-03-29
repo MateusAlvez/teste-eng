a9a5c1c843a26a553d28657683d4de82137e706d
DioguBrabo <diogoeduardo123@gmail.com>
2023-03-29 09:01:03 -0300

    teste

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,25 @@
-package com.example.demo;
+package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "user",schema = "public")
 public class User {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String postname;
+	private String email;
 
 }
